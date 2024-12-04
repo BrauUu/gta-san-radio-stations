29f0b4a0a4131ddb29ceddae045bac43b9cdd7c7
BrauUu <brayanfrank.sm@gmail.com>
2024-12-04 15:24:03 -0300

    sync radio stations with universal timer and loop

--- src/app/components/YouTubePlayer/index.js
+++ src/app/components/YouTubePlayer/index.js
@@ -1,62 +1,89 @@
 import { useEffect, useContext, useState, useRef } from "react";
-import { GlobalContext } from '../../contexts/GlobalContext';
+import { GlobalContext } from "../../contexts/GlobalContext";
 
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+function getRadioActualTime(radioDuration) {
+  return Number.parseInt(Date.now() / 1000) % Number.parseInt(radioDuration)
 }
 
 export default function YouTubePlayer({ videoId }) {
-
   const playerRef = useRef(null);
   const [player, setPlayer] = useState(null);
 
-  const {
-    setIsPlaying,
-    registerMethod,
-  } = useContext(GlobalContext);
+  const { setIsPlaying, registerMethod } = useContext(GlobalContext);
+
+  const loadVideoAsync = async (videoId) => {
+    if (!player) throw new Error("Player não inicializado");
+
+    player.loadVideoById(videoId);
+
+    return new Promise((resolve, reject) => {
+      const onPlayerStateChange = (event) => {
+        if (event.data === window.YT.PlayerState.PLAYING) {
+          resolve();
+        }
+        if (event.data === window.YT.PlayerState.ENDED) {
+          player.seekTo(
+            getRadioActualTime(player.getDuration())
+          )
+          resolve()
+        }
+      };
+
+      player.addEventListener("onStateChange", onPlayerStateChange);
+    });
+  };
 
   useEffect(() => {
-    if (!window.YT) {
-      const script = document.createElement("script");
-      script.id = 'youtube-player'
-      script.src = "https://www.youtube.com/iframe_api";
-      script.async = true;
-      script.onload = () => {
-        window.onYouTubeIframeAPIReady = () => {
-          new window.YT.Player(playerRef.current, {
-            height: "315",
-            width: "560",
-            videoId: videoId,
-            events: {
-              onReady: (event) => {
-                setPlayer(event.target);
-                event.target.playVideo()
-                event.target.seekTo(randomIntFromInterval(0, event.target.getDuration()))
-                registerMethod("play", () => {
-                  if (event.target) {
-                    event.target.playVideo();
-                    setIsPlaying(true)
-                  }
-                });
-                registerMethod("pause", () => {
-                  if (event.target) {
-                    event.target.pauseVideo();
-                    setIsPlaying(false)
-                  }
-                });
-              }
-            },
-          });
+    const initializePlayer = async () => {
+      if (!window.YT) {
+        const script = document.createElement("script");
+        script.src = "https://www.youtube.com/iframe_api";
+        script.async = true;
+        script.onload = () => {
+          window.onYouTubeIframeAPIReady = async () => {
+            new window.YT.Player(playerRef.current, {
+              height: "315",
+              width: "560",
+              videoId: videoId,
+              playerVars: {
+                loop: 1,
+                playlist: videoId,
+              },
+              events: {
+                onReady: async (event) => {
+                  setPlayer(event.target)
+                  event.target.seekTo(
+                    getRadioActualTime(event.target.getDuration())
+                  );
+                  registerMethod("play", () => {
+                    if (event.target) {
+                      event.target.playVideo();
+                      setIsPlaying(true);
+                    }
+                  });
+
+                  registerMethod("pause", () => {
+                    if (event.target) {
+                      event.target.pauseVideo();
+                      setIsPlaying(false);
+                    }
+                  });
+                },
+              },
+            });
+          };
         };
-      };
-      document.body.appendChild(script);
-    } else if (window.YT) {
-      player.loadVideoById(videoId)
-    }
+        document.body.appendChild(script);
+      } else {
+        await loadVideoAsync(videoId);
+        player.seekTo(
+          getRadioActualTime(player.getDuration())
+        )
+      }
+    };
+
+    initializePlayer();
   }, [videoId]);
 
-  return (
-    <div ref={playerRef}>
-    </div>
-  );
-};
+  return <div ref={playerRef}></div>;
+}
