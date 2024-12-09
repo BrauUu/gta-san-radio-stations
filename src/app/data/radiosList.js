f5af5cd894d6e465905a816e90c2c42173e6d5e7
BrauUu <brayanfrank.sm@gmail.com>
2024-12-09 11:02:05 -0300

    all songs data

--- src/app/data/radiosList.js
+++ src/app/data/radiosList.js
@@ -318,91 +318,91 @@ export default [
                 "name": "All My Ex's Live in Texas",
                 "author": "Whitey Shafer",
                 "startTime": 7,
-                "endTime": 0
+                "endTime": 202
             },
             {
                 "name": "One Step Forward",
                 "author": "The Desert Rose Band",
                 "startTime": 252,
-                "endTime": 0
+                "endTime": 428
             },
             {
                 "name": "New York City",
                 "author": "The Statler Brothers",
                 "startTime": 439,
-                "endTime": 0
+                "endTime": 602
             },
             {
                 "name": "Always Wanting You",
                 "author": "Merle Haggard",
                 "startTime": 646,
-                "endTime": 0
+                "endTime": 828
             },
             {
                 "name": "I Love a Rainy Night",
                 "author": "Eddie Rabbitt",
                 "startTime": 829,
-                "endTime": 0
+                "endTime": 981
             },
             {
                 "name": "Amos Moses",
                 "author": "Jerry Reed",
                 "startTime": 995,
-                "endTime": 0
+                "endTime": 1127
             },
             {
                 "name": "The Letter That Johnny Walker Read",
                 "author": "Asleep at the Wheel",
                 "startTime": 1160,
-                "endTime": 0
+                "endTime": 1342
             },
             {
                 "name": "Make the World Go Away",
                 "author": "Mickey Gilley",
                 "startTime": 1343,
-                "endTime": 0
+                "endTime": 1501
             },
             {
                 "name": "Crazy",
                 "author": "Willie Nelson",
                 "startTime": 1609,
-                "endTime": 0
+                "endTime": 1843
             },
             {
                 "name": "Three Cigarettes in an Ashtray",
                 "author": "Patsy Cline",
                 "startTime": 1844,
-                "endTime": 0
+                "endTime": 1976
             },
             {
                 "name": "Louisiana Woman, Mississippi Man",
                 "author": "Conway Twitty and Loretta",
                 "startTime": 1994,
-                "endTime": 0
+                "endTime": 2132
             },
             {
                 "name": "Mammas Don't Let Your Babies Grow Up to Be Cowboys",
                 "author": "Ed Bruce",
                 "startTime": 2145,
-                "endTime": 0
+                "endTime": 2332
             },
             {
                 "name": "Hey, Good Lookin'",
                 "author": "Hank Williams",
                 "startTime": 2333,
-                "endTime": 0
+                "endTime": 2504
             },
             {
                 "name": "Queen of Hearts",
                 "author": "Juice Newton",
                 "startTime": 2589,
-                "endTime": 0
+                "endTime": 2776
             },
             {
                 "name": "Bed of Rose's",
                 "author": "The Statler Brothers",
                 "startTime": 2795,
-                "endTime": 0
+                "endTime": 2938
             }
         ]
 
@@ -411,7 +411,106 @@ export default [
         "id": 4,
         "name": "K-JAH West",
         "video-url": "CCk3ZBv3KkA",
-        "image": "KJAHWest.png"
+        "image": "KJAHWest.png",
+        "songs": [
+            {
+                "name": "Wicked Inna Bed",
+                "author": "Shabba Ranks",
+                "startTime": 11,
+                "endTime": 204
+            },
+            {
+                "name": "Bam Bam",
+                "author": "Pliers",
+                "startTime": 232,
+                "endTime": 474
+            },
+            {
+                "name": "Ring My Bell",
+                "author": "Blood Sisters",
+                "startTime": 475,
+                "endTime": 687
+            },
+            {
+                "name": "Armagideon Time",
+                "author": "Will Williams",
+                "startTime": 710,
+                "endTime": 851
+            },
+            {
+                "name": "Don't Let It Go to Your Head",
+                "author": "Black Harmony",
+                "startTime": 949,
+                "endTime": 1173
+            },
+            {
+                "name": "Batty Rider",
+                "author": "Buju Banton",
+                "startTime": 1180,
+                "endTime": 1397
+            },
+            {
+                "name": "Here I Come",
+                "author": "Barrington Levy",
+                "startTime": 1422,
+                "endTime": 1629
+            },
+            {
+                "name": "Drum Pan Sound",
+                "author": "Reggie Stepper",
+                "startTime": 1641,
+                "endTime": 1833
+            },
+            {
+                "name": "Pressure Drop",
+                "author": "Toots & The Maytals",
+                "startTime": 1834,
+                "endTime": 2045
+            },
+            {
+                "name": "Sidewalk Killer",
+                "author": "I-Roy",
+                "startTime": 2075,
+                "endTime": 2289
+            },
+            {
+                "name": "Funky Kingston",
+                "author": "Toots & The Maytals",
+                "startTime": 2325,
+                "endTime": 2530
+            },
+            {
+                "name": "Cocaine In My Brain",
+                "author": "Dillinger",
+                "startTime": 2556,
+                "endTime": 2760
+            },
+            {
+                "name": "King Tubby Meets the Rockers Uptown",
+                "author": "Augustus Pablo",
+                "startTime": 2767,
+                "endTime": 2917
+            },
+            {
+                "name": "Great Train Robbery",
+                "author": "Black Uhuru",
+                "startTime": 2918,
+                "endTime": 3147
+            },
+            {
+                "name": "Chase the Devil",
+                "author": "Max Romeo & The Upsetters",
+                "startTime": 3220,
+                "endTime": 3419
+            },
+            {
+                "name": "Revolution",
+                "author": "Dennis Brown",
+                "startTime": 3425,
+                "endTime": 3653
+            }
+        ]
+
     },
     {
         "id": 5,
@@ -533,30 +632,404 @@ export default [
         "id": 6,
         "name": "Playback",
         "video-url": "uvkkN2REaXQ",
-        "image": "Playback.png"
+        "image": "Playback.png",
+        "songs": [
+            {
+                "name": "Children's Story",
+                "author": "Slick Rick",
+                "startTime": 8,
+                "endTime": 241
+            },
+            {
+                "name": "B.Y.S.",
+                "author": "Gang Starr",
+                "startTime": 306,
+                "endTime": 488
+            },
+            {
+                "name": "Rebel Without a Pause",
+                "author": "Public Enemy",
+                "startTime": 506,
+                "endTime": 731
+            },
+            {
+                "name": "Brand Nubian",
+                "author": "Brand Nubian",
+                "startTime": 732,
+                "endTime": 920
+            },
+            {
+                "name": "Vapors",
+                "author": "Biz Markie",
+                "startTime": 994,
+                "endTime": 1217
+            },
+            {
+                "name": "Critical Beatdown",
+                "author": "Ultramagnetic Mcs",
+                "startTime": 1225,
+                "endTime": 1421
+            },
+            {
+                "name": "Me and the Biz",
+                "author": "Masta Ace",
+                "startTime": 1483,
+                "endTime": 1695
+            },
+            {
+                "name": "Road to the Riches",
+                "author": "Kool G Rap & DJ Polo",
+                "startTime": 1707,
+                "endTime": 1928
+            },
+            {
+                "name": "Warm It Up, Kane",
+                "author": "Big Daddy Kane",
+                "startTime": 1929,
+                "endTime": 2115
+            },
+            {
+                "name": "It Takes Two",
+                "author": "Rob Base & DJ EZ Rock",
+                "startTime": 2180,
+                "endTime": 2395
+            },
+            {
+                "name": "The Godfather",
+                "author": "Spoonie Gee",
+                "startTime": 2456,
+                "endTime": 2661
+            },
+            {
+                "name": "I Know You Got Soul",
+                "author": "Eric B. & Rakim",
+                "startTime": 2662,
+                "endTime": 2866
+            }
+        ]
+
     },
     {
         "id": 7,
         "name": "Radio Los Santos",
         "video-url": "nQByeQ8f6Fk",
-        "image": "RadioLosSantos.png"
+        "image": "RadioLosSantos.png",
+        "songs": [
+            {
+                "name": "La Raza",
+                "author": "Kid Frost",
+                "startTime": 22,
+                "endTime": 219
+            },
+            {
+                "name": "Alwayz Into Somethin'",
+                "author": "N.W.A",
+                "startTime": 230,
+                "endTime": 438
+            },
+            {
+                "name": "It Was a Good Day",
+                "author": "Ice Cube",
+                "startTime": 474,
+                "endTime": 673
+            },
+            {
+                "name": "Fuck wit Dre Day (And Everybody's Celebratin')",
+                "author": "Dr. Dre feat. Snoop Dogg, RBX & Jewell",
+                "startTime": 685,
+                "endTime": 918
+            },
+            {
+                "name": "Express Yourself",
+                "author": "N.W.A",
+                "startTime": 919,
+                "endTime": 1156
+            },
+            {
+                "name": "Deep Cover",
+                "author": "Dr. Dre feat. Snoop Dogg",
+                "startTime": 1205,
+                "endTime": 1431
+            },
+            {
+                "name": "I Don't Give a Fuck",
+                "author": "2Pac feat. Pogo",
+                "startTime": 1457,
+                "endTime": 1701
+            },
+            {
+                "name": "Eazy-Er Said Than Dunn",
+                "author": "Eazy-E",
+                "startTime": 1702,
+                "endTime": 1920
+            },
+            {
+                "name": "Murder Rap",
+                "author": "Above the Law",
+                "startTime": 1945,
+                "endTime": 2197
+            },
+            {
+                "name": "How I Could Just Kill a Man",
+                "author": "Cypress Hill",
+                "startTime": 2207,
+                "endTime": 2441
+            },
+            {
+                "name": "It's Funky Enough",
+                "author": "The D.O.C.",
+                "startTime": 2467,
+                "endTime": 2696
+            },
+            {
+                "name": "Nuthin' but a 'G' Thang",
+                "author": "Dr. Dre feat. Snoop Dogg",
+                "startTime": 2723,
+                "endTime": 2938
+            },
+            {
+                "name": "Check Yo Self (Remix)",
+                "author": "Ice Cube feat. Das EFX",
+                "startTime": 2947,
+                "endTime": 3149
+            },
+            {
+                "name": "The Ghetto",
+                "author": "Too $hort",
+                "startTime": 3159,
+                "endTime": 3379
+            },
+            {
+                "name": "Hood Took Me Under",
+                "author": "Compton's Most Wanted",
+                "startTime": 3408,
+                "endTime": 3613
+            },
+            {
+                "name": "Guerillas in tha Mist",
+                "author": "Da Lench Mob feat. Ice Cube",
+                "startTime": 3625,
+                "endTime": 3820
+            }
+        ]
+
     },
     {
         "id": 8,
         "name": "Radio:X",
         "video-url": "LKo2DoOb5nc",
-        "image": "RadioX.png"
+        "image": "RadioX.png",
+        "songs": [
+            {
+                "name": "Unsung",
+                "author": "Helmet",
+                "startTime": 12,
+                "endTime": 213
+            },
+            {
+                "name": "Cult of Personality",
+                "author": "Living Colour",
+                "startTime": 224,
+                "endTime": 414
+            },
+            {
+                "name": "Plush",
+                "author": "Stone Temple Pilots",
+                "startTime": 441,
+                "endTime": 658
+            },
+            {
+                "name": "Rusty Cage",
+                "author": "Soundgarden",
+                "startTime": 685,
+                "endTime": 858
+            },
+            {
+                "name": "Welcome to the Jungle",
+                "author": "Guns N' Roses",
+                "startTime": 859,
+                "endTime": 1100
+            },
+            {
+                "name": "Mother",
+                "author": "Danzig",
+                "startTime": 1159,
+                "endTime": 1342
+            },
+            {
+                "name": "Killing in the Name",
+                "author": "Rage Against the Machine",
+                "startTime": 1397,
+                "endTime": 1690
+            },
+            {
+                "name": "Been Caught Stealing",
+                "author": "Jane's Addiction",
+                "startTime": 1752,
+                "endTime": 1952
+            },
+            {
+                "name": "Fools Gold",
+                "author": "The Stone Roses",
+                "startTime": 1953,
+                "endTime": 2200
+            },
+            {
+                "name": "Pretend We're Dead",
+                "author": "L7",
+                "startTime": 2230,
+                "endTime": 2457
+            },
+            {
+                "name": "Them Bones",
+                "author": "Alice in Chains",
+                "startTime": 2468,
+                "endTime": 2615
+            },
+            {
+                "name": "Movin' on Up",
+                "author": "Primal Scream",
+                "startTime": 2625,
+                "endTime": 2803
+            },
+            {
+                "name": "Midlife Crisis",
+                "author": "Faith No More",
+                "startTime": 2815,
+                "endTime": 3000
+            },
+            {
+                "name": "Hellraiser",
+                "author": "Ozzy Osbourne",
+                "startTime": 3050,
+                "endTime": 3254
+            },
+            {
+                "name": "Personal Jesus",
+                "author": "Depeche Mode",
+                "startTime": 3255,
+                "endTime": 3468
+            }
+        ]
+
     },
     {
         "id": 9,
         "name": "SF-UR",
         "video-url": "SlVicJ9SZt4",
-        "image": "SFUR.png"
+        "image": "SFUR.png",
+        "songs": [
+            {
+                "name": "Voodoo Ray",
+                "author": "A Guy Called Gerald",
+                "startTime": 8,
+                "endTime": 235
+            },
+            {
+                "name": "Can You Feel It?",
+                "author": "Mr. Fingers",
+                "startTime": 264,
+                "endTime": 502
+            },
+            {
+                "name": "Move Your Body",
+                "author": "Marshall Jefferson",
+                "startTime": 503,
+                "endTime": 746
+            },
+            {
+                "name": "Let the Music (Use You)",
+                "author": "Nightwriters",
+                "startTime": 758,
+                "endTime": 1043
+            },
+            {
+                "name": "Make My Body Rock",
+                "author": "Jomanda",
+                "startTime": 1106,
+                "endTime": 1352
+            },
+            {
+                "name": "Pacific 202",
+                "author": "808 State",
+                "startTime": 1353,
+                "endTime": 1533
+            },
+            {
+                "name": "This Is Acid",
+                "author": "Maurice",
+                "startTime": 1540,
+                "endTime": 1747
+            },
+            {
+                "name": "Promised Land",
+                "author": "Joe Smooth Feat. Anthony",
+                "startTime": 1808,
+                "endTime": 1989
+            },
+            {
+                "name": "Weekend",
+                "author": "The Todd Terry Project",
+                "startTime": 1999,
+                "endTime": 2231
+            },
+            {
+                "name": "Someday",
+                "author": "CeCe Rogers",
+                "startTime": 2242,
+                "endTime": 2501
+            },
+            {
+                "name": "The Morning After",
+                "author": "Fallout",
+                "startTime": 2502,
+                "endTime": 2756
+            },
+            {
+                "name": "Break 4 Love",
+                "author": "Raze",
+                "startTime": 2767,
+                "endTime": 2993
+            },
+            {
+                "name": "I'll Be Your Friend (DEF Mix)",
+                "author": "Robert Owens",
+                "startTime": 3047,
+                "endTime": 3325
+            },
+            {
+                "name": "Ma Foom Bey",
+                "author": "Cultural Vibe",
+                "startTime": 3326,
+                "endTime": 3559
+            },
+            {
+                "name": "Your Love",
+                "author": "Frankie Knuckles",
+                "startTime": 3570,
+                "endTime": 3838
+            },
+            {
+                "name": "I Need a Rhythm",
+                "author": "The 28th Street Crew",
+                "startTime": 3846,
+                "endTime": 4084
+            }
+        ]
+
     },
     {
         "id": 10,
         "name": "WCTR",
         "video-url": "ZgeHsKbGJbU",
-        "image": "WCTR.png"
+        "image": "WCTR.png",
+        "songs": [
+            {
+                "name": "Speaking",
+                "author": null,
+                "startTime": 0,
+                "endTime": 7545
+            }  
+        ]
     },
 ]
